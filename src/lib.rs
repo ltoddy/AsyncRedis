22fa4c6af1bbbfefd69ec982c458ac8109007bc1
ltoddy <taoliu0509@gmail.com>
2022-01-24 23:42:02 +0800

    feat: read-write separation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,37 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::io;
+
+use tokio::io::{split, AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter, ReadHalf, WriteHalf};
+use tokio::net::{TcpStream, ToSocketAddrs};
+
+pub struct Connection {
+    reader: BufReader<ReadHalf<TcpStream>>,
+    writer: BufWriter<WriteHalf<TcpStream>>,
+}
+
+impl Connection {
+    pub async fn connect<A>(addr: A) -> io::Result<Self>
+    where
+        A: ToSocketAddrs,
+    {
+        fn __new(stream: TcpStream) -> Connection {
+            let (reader, writer) = split(stream);
+            let reader = BufReader::new(reader);
+            let writer = BufWriter::new(writer);
+            Connection { reader, writer }
+        }
+
+        let stream = TcpStream::connect(addr).await?;
+        Ok(__new(stream))
+    }
+
+    pub async fn send(&mut self, data: &[u8]) -> io::Result<()> {
+        self.writer.write_all(data).await?;
+        Ok(())
+    }
+
+    pub async fn receive(&mut self) -> io::Result<Vec<u8>> {
+        let mut buffer = Vec::with_capacity(128);
+        self.reader.read_until(b'\n', &mut buffer).await?;
+        Ok(buffer)
     }
 }
